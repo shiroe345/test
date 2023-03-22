e47eb0cd2d40cdb36ce317289d120571ed0e27c5
shiroe345 <gq4575@gmail.com>
2023-03-22 16:08:46 +0800

    first change

---
+++ test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "abc.cpp"
+using namespace std;
+
+void testing(int i){
+    cout<<i<<" ";   
+}
+int main(){
+    int i = 0;
+    testing(++i);
+    // A a = *new A();
+    // cout<<a.a<<" "<<a.C();
+    test();
+    return 0;
+}
