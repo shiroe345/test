ed05ebceab324759a136ed2d20d5698415f649eb
shiroe345 <gq4575@gmail.com>
2023-12-01 17:05:53 +0800

    cd AI_C

--- abc.cpp
+++ abc.cpp
@@ -15,6 +15,7 @@ class A{
 void test(){
     cout<<"hello world";
     cout<<"hello two branch here!"<<endl;
-    cout<<"hello world one herdsdfsfsfsdfsfsdfsfsfsfe!"<<endl;
+    cout<<"hello world one herdsd!"<<endl;
     cout<<"hello branchhh one!"<<endl;
 }
+
