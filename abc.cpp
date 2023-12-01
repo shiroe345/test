3d670613c660c1d19875768bcb200cd98c540912
shiroe345 <gq4575@gmail.com>
2023-12-01 16:44:53 +0800

    123

--- abc.cpp
+++ abc.cpp
@@ -14,7 +14,5 @@ class A{
 };
 void test(){
     cout<<"hello world";
-    cout<<"hello two branch here!"<<endl;
-    cout<<"hello world one herdsfe!"<<endl;
-    cout<<"hello branchhh one!"<<endl;
+    cout<<"hello branch odsfne hello!"<<endl;
 }
